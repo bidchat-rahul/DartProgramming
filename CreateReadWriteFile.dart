67921589690534134854822d297b7d573c3c0365
Rahul Harmalkar <rahul.harmalkar@bidchat.com>
2018-08-20 18:45:34 +0530

    fileReadWrite

---
+++ CreateReadWriteFile.dart
@@ -0,0 +1,40 @@
+// file created on 20/08/2018
+// by Rahul Harmalkar
+import 'dart:io';
+import 'dart:convert';
+import 'dart:async';
+
+main(){
+  // code to create and write to the file
+  String path = "./CreatedTestFile.txt";
+  var myFile = new File(path);
+  var fileStream = myFile.openWrite();
+
+  fileStream.write("Hi, this is the content generated from the code2");
+
+  fileStream.close();
+
+  // code to read from the file that has been created using stream
+  Stream<List<int>> fileStreamRead = myFile.openRead();
+
+  fileStreamRead.transform(utf8.decoder)
+  .transform(new LineSplitter())
+  .listen((data){
+    print('using stream: $data');
+  },
+  onDone: (){},
+  onError: (err){
+    print(err.toString());
+    }
+  );
+  
+  // code to read from the file that has been created without using stream
+  myFile.readAsString().then((String contexts){
+    print('without using stream: $contexts');
+  });
+
+  myFile.exists().then((isPresent){
+    // prints boolean
+    print(isPresent);
+  });
+}
